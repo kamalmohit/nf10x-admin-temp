c7f27561a0f47f52b728f5dd20ae6c4091c58dfc
mohitkamal-grappus <92913797+mohitkamal-grappus@users.noreply.github.com>
2022-04-01 15:06:24 +0530

    Removing dead deps

--- src/components/DashboardHeader/DashboardHeader.jsx
+++ src/components/DashboardHeader/DashboardHeader.jsx
@@ -18,9 +18,13 @@ const DashboardHeader = () => {
                 return `/home/${key}`;
         }
     }
+
+    const handleHomeNavigation = () => {
+        navigate('/home/users/whitelist');
+    }
     return (
         <header className="sticky top-0 left-0 z-10 w-full flex flex-row justify-between bg-black text-grey h-14 pl-6 pr-6">
-            <span className="self-center flex items-center">
+            <span className="cursor-pointer" onClick={handleHomeNavigation} className="self-center flex items-center">
                 <img src={Logo} alt="Logo small" className="mr-2" />NF10X
             </span>
             <ul className="flex flex-row basis-1/3 justify-between pl-1 pr-1 pt-4 text-md font-normal">
